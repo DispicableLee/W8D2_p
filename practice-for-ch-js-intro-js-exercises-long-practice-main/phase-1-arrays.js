510a2a9a1d606d7a1dbd2fdbb2fd0ad8e68fcc97
DispicableLee <lee.robert053@gmail.com>
2023-10-10 13:57:03 -0400

    com

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
@@ -1,4 +1,9 @@
 // Your code here
-Array.prototype.uniq = () =>{
-    
+Array.prototype.uniq = (arr) =>{
+    let returned = []
+    for(let i = 0; i<arr.length; i++){
+        if returned.includes(arr[i]){
+            
+        }
+    }
 }
