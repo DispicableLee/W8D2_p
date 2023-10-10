623e84ee4598bbb71e38e6b0ae9973c1b01bc1e7
DispicableLee <lee.robert053@gmail.com>
2023-10-10 16:53:28 -0400

    suffering

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-4-recursion.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-4-recursion.js
@@ -8,5 +8,28 @@ function range(start, end) {
 
 }
 
-console.log(range(0,10));
+// console.log(range(0,10));
+
+function sumRec(arr){
+    if(arr.length===1){
+        return arr[0];
+    }
+
+    return arr[0] + sumRec(arr.slice(1)); 
+}
+
+// console.log(sumRec([]))
+
+function exponent(base, exp){
+    if(exp ===1){
+        return base
+    }else if(exp<1){
+        return 1
+    }
+
+    return base * exponent(base, exp-1)
+
+}
+
+// console.log(exponent(5,3)) 
 
