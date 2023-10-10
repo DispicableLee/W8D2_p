821afa39be41b4ecfd0f3e12b5f89431b0c23d17
yuris <yls4@cornell.edu>
2023-10-10 16:33:06 -0400

    finishd bubble sort

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-3-iteration.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-3-iteration.js
@@ -1,1 +1,18 @@
-// Your code here
+Array.prototype.bubbleSort = function() {
+    let sorted = true
+    while (sorted) {
+        sorted = false;
+        for (let i=0;i < this.length; i++) {
+            let next = i + 1;
+            if (this[i] > this[next]) {
+                let temp = this[next];
+                this[next] = this[i];
+                this[i] = temp;
+                sorted = true;
+            }
+        }
+    }
+    return this
+}
+
+console.log([1,3,5,1,2].bubbleSort())
