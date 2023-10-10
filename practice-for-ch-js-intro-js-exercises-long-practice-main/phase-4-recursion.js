6d21495a77b0c3999aa68867270c8cc494f03cb5
yuris <yls4@cornell.edu>
2023-10-10 16:45:42 -0400

    finished recursion problem 1

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-4-recursion.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-4-recursion.js
@@ -1,1 +1,12 @@
-// Your code here
+function range(start, end) {
+    if (start === end) {
+        return end;
+    }
+
+    let temp = [start];
+    return temp.concat(range(start+1, end));
+
+}
+
+console.log(range(0,10));
+
